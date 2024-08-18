688a7d0ebceda37280cfa1abb46f1a2c016b1dcb
Your Name <khalidsaifullaha3@gmail.com>
2024-08-18 12:46:44 +0600

    great tea section done

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,3 +6,19 @@ module.exports = {
     },
     plugins: [],
   }
+//   <div class="">
+//   <h1>Great Tea, Freshly Presented</h1>
+//   <p class="">
+//     The meaning of gong cha is chanese is to provide the best tea to emperor from all possessions . It represents the highest quality and self expectation. Establishing in 2006, Gong cha had been deeply appreciated by its custoers  because of good words of mouth and unique customized service orginated from Taiwan.
+//   </p>
+//   <br>
+//   <h1 class="">Unique Taste</h1>
+//   <p class="">
+//     A Unique and different style from other teapots gives a luxurious and minimalist impression
+//   </p>
+//   <br>
+//   <h1 class="">Premium Quality</h1>
+//   <p class="">
+//     Premium Quality that makes tea more elegant and more durable when you use it.
+//   </p>
+//  </div>
